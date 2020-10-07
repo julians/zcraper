62255c35d514e9074afba3a5f52f240ea849536f
Julian Stahnke <hello@julianstahnke.com>
2020-10-07 13:53:50 +0200

    portable cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-cd /home/jujulian/code/zcraper
+script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $script_path
+
 source venv/bin/activate
 python scrape.py
 python bot.py
